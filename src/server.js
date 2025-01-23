ee49458c67d751f0645615360d9fee758d5cc97e
Aviv <Avivsh0204@gmail.com>
2025-01-23 17:13:53 +0200

    [server] almost complete

--- src/server.js
+++ src/server.js
@@ -20,7 +20,49 @@ app.listen(PORT, () => {
 
 
 async function handleLiveEvent(req, res) {
+    try {
+        const authHeader = req.headers.authorization;
+        if (authHeader !== 'secret') {
+            return res.status(401).send('Unauthorized');
+        }
 
+        await processEvent(req.body);
+        res.status(200).send('Event Handled...');
+    } catch (err) {
+        res.status(500).send(err);
+    }
 }
 
 
+async function processEvent(event) {
+    const fileName = 'events-storage.txt';
+    await fs.appendFile(fileName, JSON.stringify(event) + '\n');
+}
+
+
+async function getUserEvent(req, res) {
+    try {
+        const authHeader = req.headers.authorization;
+        const { userId } = req.params;
+        if (authHeader !== 'secret') {
+            return res.status(401).send('Unauthorized');
+        }
+        if (!userId) {
+            return res.status(400).send('User ID is required');
+        }
+
+        const result = await getUserFromDB(userId);
+        if (result.rows.length === 0) {
+            return res.status(404).send('User not found');
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+
+async function getUserFromDB(userId) {
+    return client.query('SELECT * FROM users_revenue WHERE user_id = $1', [userId]);
+}
